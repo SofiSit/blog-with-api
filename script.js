6635d220c8d1e32237037df506946be420d6a820
Albert Gomis <albert.gomis.20@gmail.com>
2022-12-15 09:31:32 +0100

    delete y scroll correcto

--- script.js
+++ script.js
@@ -39,46 +39,27 @@ const morePostsBtn = document.querySelector("#morePosts");
   
 //RENDERIZAR POST EN DIV
 function  listPost(post, cont, div) {
-    let content = document.getElementById("card-post");
-    if (cont === 0) {
-      div.className = "row";
-  
-      div.innerHTML += `
-        <div class="col-lg-4 col-md-6 col-sm-12">
-          <div class="card m-auto my-2" style="">
-            
-            <div class="card-body position-relative cursor-pointer" data-id="${post.id}" data-bs-toggle="modal" data-bs-target="#exampleModal">
-
-             <img src="https://picsum.photos/id/${post.id}/200/200 " class="card-img-top cursor-pointer blur" alt="..." data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="${post.id}"> 
+  let content = document.getElementById("card-post");
+  console.log(cont)
+  if (cont < 99) {
+    div.className = "row";
+    div.innerHTML +=
+      `<div class="col-lg-4 col-md-6 col-sm-12">
+        <div class="card m-auto my-2" style="">
           
-              <h5 class="card-title card__letters title__post" data-id="${post.id}">${post.title}</h5>
-              <!-- <p class="card-text">${post.body}</p>-->
-            </div>
+          <div class="card-body position-relative cursor-pointer" data-id="${post.id}" data-bs-toggle="modal" data-bs-target="#exampleModal">
+
+           <img src="https://picsum.photos/id/${post.id}/200/200 " class="card-img-top cursor-pointer blur" alt="..." data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="${post.id}"> 
+        
+            <h5 class="card-title card__letters title__post" data-id="${post.id}">${post.title}</h5>
+            <!-- <p class="card-text">${post.body}</p>-->
           </div>
         </div>
-      `;
-      content.appendChild(div);
-    }
-  
-    else {
-     let laster = document.querySelectorAll(".row").length - 1;
-    document.querySelectorAll(".row")[laster].innerHTML += `
-    <div class="col-lg-4 col-md-6 col-sm-12">
-      <div class="card m-auto my-2" style="">
-        
-        <div class="card-body position-relative cursor-pointer" data-id="${post.id}" data-bs-toggle="modal" data-bs-target="#exampleModal">
-
-      <img src="https://picsum.photos/id/${post.id}/200/200 " class="card-img-top cursor-pointer blur" alt="..." data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="${post.id}"> 
-       
-          <h5 class="card-title card__letters title__post" data-id="${post.id}">${post.title}</h5>
-          <!--<p class="card-text">${post.body}</p>-->
-        </div>
-      </div>
-    </div>
-  ` 
-      ;
-    }
+      </div>`
+    ;
+    content.appendChild(div);
   }
+}
  //CLICK IMAGEN POST 
   document.addEventListener("click", function (e) {
     if (e.target.matches("[data-id]")) infoModal(e);
@@ -157,11 +138,6 @@ function readComments(comments){
     });
   }
 
-  // fetch(`http://localhost:3000/posts/2/comments`)
-  // .then((response) => response.json())
-  // .then((comments) => {
-  //   console.log(comments);
-  // })
 
   const confirmSuprimir = document.querySelector("#confirm-delete-icon");
 
